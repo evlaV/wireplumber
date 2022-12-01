9550e357db38f4ed2c7c589d967450519ac0a5a3
Julian Bouzas <julian.bouzas@collabora.com>
2022-12-01 13:17:31 -0500

    access-default.lua: dont give several filter nodes permissions to pipewire-pulse client
    This showing echo-cancel and filter-chain nodes in all pulseaudio applications.

--- src/scripts/access/access-default.lua
+++ src/scripts/access/access-default.lua
@@ -34,6 +34,9 @@ function rulesGetDefaultPermissions(properties)
   end
 end
 
+nodes_om = ObjectManager { Interest { type = "node" } }
+nodes_om:activate()
+
 clients_om = ObjectManager {
   Interest { type = "client" }
 }
@@ -42,8 +45,34 @@ clients_om:connect("object-added", function (om, client)
   local id = client["bound-id"]
   local properties = client["properties"]
 
-  local perms = rulesGetDefaultPermissions(properties)
+  -- do not give any permission to filter nodes if client is pipewire-pulse
+  if properties["config.name"] == "pipewire-pulse.conf" then
+    local perms_table = { ["any"] = "all" }
 
+    for n in nodes_om:iterate() do
+      local node_id = n["bound-id"]
+      local node_properties = n["properties"]
+      if node_properties["node.name"] == "echo-cancel-sink" or
+          node_properties["node.name"] == "echo-cancel-source" or
+          node_properties["node.name"] == "echo-cancel-playback" or
+          node_properties["node.name"] == "echo-cancel-capture" or
+          node_properties["node.name"] == "filter-chain-sink" or
+          node_properties["node.name"] == "filter-chain-source" or
+          node_properties["node.name"] == "filter-chain-playback" or
+          node_properties["node.name"] == "filter-chain-capture" or
+          node_properties["node.name"] == "input.virtual-source" then
+        Log.info (client, "Removing permissions to client " .. id .. " in node " .. node_id)
+        perms_table[node_id] = ""
+      end
+    end
+
+    Log.info(client, "Granting permissions to client " .. id .. ": " .. perms_table["any"])
+    client:update_permissions (perms_table)
+    return
+  end
+
+
+  local perms = rulesGetDefaultPermissions(properties)
   if perms then
     Log.info(client, "Granting permissions to client " .. id .. ": " .. perms)
     client:update_permissions { ["any"] = perms }
